ca2499e21cbedccc34aa42a8e071ee4ba7a4d1aa
Martijn Courteaux <courteauxmartijn@gmail.com>
2015-07-03 19:36:53 +0200

    Bugfix when switching to other desktop. Some renaming details.

--- src/main/java/com/martijncourteaux/multitouchgestures/MultiTouchGestureUtilities.java
+++ src/main/java/com/martijncourteaux/multitouchgestures/MultiTouchGestureUtilities.java
@@ -15,12 +15,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import javax.swing.JComponent;
 import java.util.List;
+
 /**
  *
  * @author martijn
  */
 public class MultiTouchGestureUtilities
 {
+
     private final static HashMap<JComponent, List<GestureListener>> listeners = new HashMap<JComponent, List<GestureListener>>();
     private static int listenerCount = 0;
 
@@ -28,7 +30,7 @@ public class MultiTouchGestureUtilities
     {
         return listenerCount;
     }
-    
+
     public static void addGestureListener(JComponent component, GestureListener listener)
     {
         if (listenerCount == 0)
@@ -41,11 +43,11 @@ public class MultiTouchGestureUtilities
             list = new ArrayList<GestureListener>();
             listeners.put(component, list);
         }
-        
+
         list.add(listener);
         listenerCount++;
     }
-    
+
     public static boolean removeGestureListener(JComponent component, GestureListener listener)
     {
         List<GestureListener> list = listeners.get(component);
@@ -53,7 +55,7 @@ public class MultiTouchGestureUtilities
         {
             return false;
         }
-        
+
         if (list.remove(listener))
         {
             listenerCount--;
@@ -65,14 +67,17 @@ public class MultiTouchGestureUtilities
         }
         return false;
     }
-    
+
     protected static void dispatchMagnifyGesture(double mouseX, double mouseY, double magnification, GestureEvent.Phase phase)
     {
-        if (listenerCount == 0) return;
-        
+        if (listenerCount == 0)
+        {
+            return;
+        }
+
         int mXi = (int) Math.round(mouseX);
         int mYi = (int) Math.round(mouseY);
-        
+
         for (HashMap.Entry<JComponent, List<GestureListener>> e : listeners.entrySet())
         {
             JComponent c = e.getKey();
@@ -80,29 +85,33 @@ public class MultiTouchGestureUtilities
             if (r.contains(mXi, mYi))
             {
                 List<GestureListener> list = e.getValue();
-                
+
                 Point relP = c.getMousePosition(true);
-                
-                MagnifyGestureEvent me = new MagnifyGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, magnification);
-                
-                for (GestureListener l : list)
+                if (relP != null)
                 {
-                    l.magnify(me);
+                    MagnifyGestureEvent me = new MagnifyGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, magnification);
+
+                    for (GestureListener l : list)
+                    {
+                        l.magnify(me);
+                    }
                 }
-                
+
                 return;
             }
         }
     }
-    
+
     protected static void dispatchRotateGesture(double mouseX, double mouseY, double rotation, GestureEvent.Phase phase)
     {
-        if (listenerCount == 0) return;
-        
-        
+        if (listenerCount == 0)
+        {
+            return;
+        }
+
         int mXi = (int) Math.round(mouseX);
         int mYi = (int) Math.round(mouseY);
-        
+
         for (HashMap.Entry<JComponent, List<GestureListener>> e : listeners.entrySet())
         {
             JComponent c = e.getKey();
@@ -110,28 +119,33 @@ public class MultiTouchGestureUtilities
             if (r.contains(mXi, mYi))
             {
                 List<GestureListener> list = e.getValue();
-                
+
                 Point relP = c.getMousePosition(true);
-                
-                RotateGestureEvent re = new RotateGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, rotation);
-                
-                for (GestureListener l : list)
+                if (relP != null)
                 {
-                    l.rotate(re);
+                    RotateGestureEvent re = new RotateGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, rotation);
+
+                    for (GestureListener l : list)
+                    {
+                        l.rotate(re);
+                    }
                 }
-                
+
                 return;
             }
         }
     }
-        
+
     protected static void dispatchScrollGesture(double mouseX, double mouseY, double dX, double dY, GestureEvent.Phase phase)
     {
-        if (listenerCount == 0) return;
-        
+        if (listenerCount == 0)
+        {
+            return;
+        }
+
         int mXi = (int) Math.round(mouseX);
         int mYi = (int) Math.round(mouseY);
-        
+
         for (HashMap.Entry<JComponent, List<GestureListener>> e : listeners.entrySet())
         {
             JComponent c = e.getKey();
@@ -139,19 +153,21 @@ public class MultiTouchGestureUtilities
             if (r.contains(mXi, mYi))
             {
                 List<GestureListener> list = e.getValue();
-                
+
                 Point relP = c.getMousePosition(true);
-                
-                ScrollGestureEvent se = new ScrollGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, dX, dY);
-                
-                for (GestureListener l : list)
+                if (relP != null)
                 {
-                    l.scroll(se);
+                    ScrollGestureEvent se = new ScrollGestureEvent(c, relP.getX(), relP.getY(), mouseX, mouseY, phase, dX, dY);
+
+                    for (GestureListener l : list)
+                    {
+                        l.scroll(se);
+                    }
                 }
-                
+
                 return;
             }
         }
     }
-    
+
 }
