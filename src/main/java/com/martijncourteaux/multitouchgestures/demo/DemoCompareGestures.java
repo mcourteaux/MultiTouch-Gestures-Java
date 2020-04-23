5bdccc493cf0b62b39e197281b370b9ef5d11ed9
Martijn Courteaux <courteauxmartijn@gmail.com>
2020-04-23 15:08:00 +0200

    I don't know. Probably something usefull.

--- src/main/java/com/martijncourteaux/multitouchgestures/demo/DemoCompareGestures.java
+++ src/main/java/com/martijncourteaux/multitouchgestures/demo/DemoCompareGestures.java
@@ -29,50 +29,43 @@ import javax.swing.JFrame;
  *
  * @author martijn
  */
-public class DemoCompareGestures
-{
-    
+public class DemoCompareGestures {
     private static double a = 0, l = 50;
     private static double x, y;
-    
+
     private static double aa = 0, al = 50;
     private static double ax, ay;
-    
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         JFrame frame = new JFrame();
         frame.setTitle("MultiTouch left, Apple right");
-        final JComponent compMT = new JComponent()
-        {
-
+        final JComponent compMT = new JComponent() {
             @Override
-            protected void paintComponent(Graphics gg)
-            {
+            protected void paintComponent(Graphics gg) {
                 super.paintComponent(gg);
                 Graphics2D g = (Graphics2D) gg;
 
-                Line2D.Double line = new Line2D.Double(getWidth() * 0.5 + x, getHeight() * 0.5 + y, getWidth() * 0.5 + Math.cos(a) * l + x, getHeight() * 0.5 + Math.sin(a) * l + y);
+                Line2D.Double line = new Line2D.Double(getWidth() * 0.5 + x, getHeight() * 0.5 + y,
+                    getWidth() * 0.5 + Math.cos(a) * l + x,
+                    getHeight() * 0.5 + Math.sin(a) * l + y);
                 g.setColor(Color.red);
                 g.setStroke(new BasicStroke(5.0f));
                 g.draw(line);
             }
-
         };
-        final JComponent compApple = new JComponent()
-        {
-
+        final JComponent compApple = new JComponent() {
             @Override
-            protected void paintComponent(Graphics gg)
-            {
+            protected void paintComponent(Graphics gg) {
                 super.paintComponent(gg);
                 Graphics2D g = (Graphics2D) gg;
 
-                Line2D.Double line = new Line2D.Double(getWidth() * 0.5 + ax, getHeight() * 0.5 + ay, getWidth() * 0.5 + Math.cos(aa) * al + ax, getHeight() * 0.5 + Math.sin(aa) * al + ay);
+                Line2D.Double line = new Line2D.Double(getWidth() * 0.5 + ax,
+                    getHeight() * 0.5 + ay, getWidth() * 0.5 + Math.cos(aa) * al + ax,
+                    getHeight() * 0.5 + Math.sin(aa) * al + ay);
                 g.setColor(Color.red);
                 g.setStroke(new BasicStroke(5.0f));
                 g.draw(line);
             }
-
         };
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(1, 2));
@@ -84,67 +77,51 @@ public class DemoCompareGestures
         frame.pack();
         frame.setVisible(true);
 
-        MultiTouchGestureUtilities.addGestureListener(compMT, new GestureAdapter()
-        {
-
+        MultiTouchGestureUtilities.addGestureListener(compMT, new GestureAdapter() {
             @Override
-            public void magnify(MagnifyGestureEvent e)
-            {
+            public void magnify(MagnifyGestureEvent e) {
                 l *= 1.0 + e.getMagnification();
                 compMT.repaint();
             }
 
             @Override
-            public void rotate(RotateGestureEvent e)
-            {
+            public void rotate(RotateGestureEvent e) {
                 a += e.getRotation();
                 compMT.repaint();
             }
 
             @Override
-            public void scroll(ScrollGestureEvent e)
-            {
+            public void scroll(ScrollGestureEvent e) {
                 x += e.getDeltaX();
                 y += e.getDeltaY();
                 compMT.repaint();
             }
-
         });
-        
-        compApple.addMouseWheelListener(new MouseWheelListener()
-        {
 
+        compApple.addMouseWheelListener(new MouseWheelListener() {
             @Override
-            public void mouseWheelMoved(MouseWheelEvent e)
-            {
-                if (e.isShiftDown())
-                {
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.isShiftDown()) {
                     ax -= 3.0f * e.getPreciseWheelRotation();
-                } else
-                {
+                } else {
                     ay -= 3.0f * e.getPreciseWheelRotation();
                 }
                 compApple.repaint();
             }
         });
 
-        GestureUtilities.addGestureListenerTo(compApple, new com.apple.eawt.event.GestureAdapter()
-        {
-
+        GestureUtilities.addGestureListenerTo(compApple, new com.apple.eawt.event.GestureAdapter() {
             @Override
-            public void magnify(MagnificationEvent me)
-            {
+            public void magnify(MagnificationEvent me) {
                 al *= 1.0 + me.getMagnification();
                 compApple.repaint();
             }
 
             @Override
-            public void rotate(RotationEvent re)
-            {
+            public void rotate(RotationEvent re) {
                 aa -= Math.toRadians(re.getRotation());
                 compApple.repaint();
             }
-
         });
     }
 }
